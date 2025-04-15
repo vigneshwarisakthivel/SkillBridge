ea694d298e6c1e5d3b80ba48d94b7fc51775ca28
vigneshwari <vigneshwarisakthivel8@gmail.com>
2025-04-15 10:26:36 +0530

    bac

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -13,22 +13,23 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+from decouple import config
+SECRET_KEY = config('SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
+ALLOWED_HOSTS = ['*'] 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0zys48qt32hw@9x8vr&r!71j34_ez4qp-$zkq)m1)+#8xl=p8_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+
 
 
 # Application definition
