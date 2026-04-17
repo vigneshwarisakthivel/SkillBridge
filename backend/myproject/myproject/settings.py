07743cdd7b1212095725d82d266e4fb773617444
vigneshwarisakthivel <vigneshwarisakthivel18@gmail.com>
2026-04-17 10:05:44 +0530

    settinsge

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework_simplejwt.token_blacklist',
-    'app.apps.AppConfig',
+    'app',
 ]
 
 MIDDLEWARE = [
