0a3b082022bace1c736928732ee76d8c555a0b6a
vigneshwarisakthivel <vigneshwarisakthivel18@gmail.com>
2026-04-17 09:08:11 +0530

    commiting

--- backend/myproject/myproject/settings.py
+++ backend/myproject/myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a^_(koyu@=2f$t^q6-_et7*ug^$8+)jh_8p705h!vdq35dx(1u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
